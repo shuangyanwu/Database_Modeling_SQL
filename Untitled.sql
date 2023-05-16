04c07f9939c46a6ea4f7e0eae6324409ba2d925f
Shuangyan Wu <112211152+shuangyanwu@users.noreply.github.com>
2023-05-16 00:34:43 -0400

    Add files via upload

--- Untitled.sql
+++ Untitled.sql
@@ -1,4 +1,5 @@
-#1: An Airbnb host wants to check important rules that appear in every airbnb and show all available translated versions for those rules. Language, ruleID, and TranslatedRuleid are also shown, results are ordered by language.
+#1: An Airbnb host wants to check important rules that appear in every airbnb and show all available 
+# translated versions for those rules. Language, ruleID, and TranslatedRuleid are also shown, results are ordered by language.
 
 SELECT 
     language, ruleid, TranslatedRuleid, TranslationText
@@ -26,9 +27,10 @@ WHERE
                                 AND airbnbRule.TranslatedRuleid = TranslatedRule.TranslatedRuleid)))
 ORDER BY language;
 
-#2: The rules that have the word “pet” or “smoking” are common rules, it may be more user friendly to have these rules in several different languages. So, the numbers of their translated rules are counted and presented together with the ruleID and ruleText.
-Execute:
-> SELECT
+#2: The rules that have the word “pet” or “smoking” are common rules, it may be more user friendly to have these rules 
+# in several different languages. So, the numbers of their translated rules are counted and presented together with the ruleID and ruleText.
+
+SELECT
 rule.ruleID,
 ruleText,
 COUNT(TranslatedRuleid) AS TranslationCount
