4aa70166d27fc3a14a7f6da4fec6db857eca0e82
Shuangyan Wu <112211152+shuangyanwu@users.noreply.github.com>
2023-05-16 21:00:32 -0400

    Add files via upload

--- Untitled.sql
+++ Untitled.sql
@@ -1,5 +1,6 @@
-#1: An Airbnb host wants to check important rules that appear in every airbnb and show all available 
-# translated versions for those rules. Language, ruleID, and TranslatedRuleid are also shown, results are ordered by language.
+#1. An Airbnb host wants to check important rules that appear in every airbnb and show all available 
+# translated versions for those rules. Language, ruleID, and TranslatedRuleid are also shown, results 
+# are ordered by language.
 
 SELECT 
     language, ruleid, TranslatedRuleid, TranslationText
@@ -27,18 +28,37 @@ WHERE
                                 AND airbnbRule.TranslatedRuleid = TranslatedRule.TranslatedRuleid)))
 ORDER BY language;
 
-#2: The rules that have the word “pet” or “smoking” are common rules, it may be more user friendly to have these rules 
-# in several different languages. So, the numbers of their translated rules are counted and presented together with the ruleID and ruleText.
+Output:
++ ------------- + ----------- + ------------------- + -------------------- +
+| language      | ruleid      | TranslatedRuleid      | TranslationText      |
++ ------------- + ----------- + ------------------- + -------------------- +
+| english       | 81101       | 61101               | No pets allowed      |
+| english       | 81102       | 61102               | No smoking           |
+| german        | 81101       | 61109               | Keine Haustiere erlaubt. |
+| german        | 81102       | 61110               | Rauchen im Apartment ist verboten. |
+| spanish       | 81101       | 61105               | No se admiten animales de compañía |
+| spanish       | 81102       | 61106               | No fumar             |
++ ------------- + ----------- + ------------------- + -------------------- +
 
-SELECT
-rule.ruleID,
-ruleText,
-COUNT(TranslatedRuleid) AS TranslationCount
+
+#2. The rules that have the word “pet” or “smoking” are common rules, it may be more user friendly to 
+# have these rules in several different languages. So, the numbers of their translated rules are counted
+# and presented together with the ruleID and ruleText.
+
+SELECT 
+    rule.ruleID,
+    ruleText,
+    COUNT(TranslatedRuleid) AS TranslationCount
 FROM
-TranslatedRule,
-rule WHERE
-rule.ruleID = TranslatedRule.ruleId AND rule.ruleID IN (SELECT
-rule.ruleID FROM
-rule WHERE
-ruleText REGEXP 'pet|smoking') GROUP BY rule.ruleID;
+    TranslatedRule,
+    rule
+WHERE
+    rule.ruleID = TranslatedRule.ruleId
+        AND rule.ruleID IN (SELECT 
+            rule.ruleID
+        FROM
+            rule
+        WHERE
+            ruleText REGEXP 'pet|smoking')
+GROUP BY rule.ruleID;
 
